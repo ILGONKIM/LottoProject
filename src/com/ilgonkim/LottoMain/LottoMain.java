b81c3e06e077ad873df26bd618a8f38d3ab5b409
ILGONKIM <ilgon.kim@gmail.com>
2018-06-07 17:18:35 +0900

    주석수정

--- src/com/ilgonkim/LottoMain/LottoMain.java
+++ src/com/ilgonkim/LottoMain/LottoMain.java
@@ -3,6 +3,7 @@ package com.ilgonkim.LottoMain;
 public class LottoMain {
 
 	public static void main(String[] args) {
+		// 주석을 추가했습니다.
 		
 		Lotto lotto = new Lotto();
 		int numbers[]  = lotto.generate();
